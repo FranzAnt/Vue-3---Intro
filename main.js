641930829cfaede6ea9676df39f80c30563b300f
Franz Bendezu <franzant20@gmail.com>
2021-09-05 15:45:19 -0500

    ovewview of vue options API

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
+// Usando el API Options
+const options = {
+    // esta funcion siempre retorna un objeto
+	data: () => ({
+      name: "Mi primera aplicacación con Vue"
+    
+    }),
+    // cuando se crea objeto,  solo se ejecuta un vez
+    created(){
+
+    },
+    // cuando se monta en el navegador,  solo se ejecuta un vez
+    mounted(){
+
+    },
+    methods: {
+        foo(){
+
+        }
+    },
+    computed:{
+
+    },
+    watch:{
+
+    }
+}
+
 const app = ''
